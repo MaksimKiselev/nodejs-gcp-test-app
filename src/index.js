e483a3fc0a3780684822648eb2b107d531633c4f
Maksim Kiselev <m.kiselev@leningrad.io>
2019-03-01 18:05:55 +0300

    Refactor application: page views counter separated in module

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
+import PageViewsCounter from './PageViewsCounter';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const pageViewsCounter = new PageViewsCounter();
 
 let pageViews = [];
 
 app.get('*', function (req, res) {
-    let now = Date.now();
-    let pastMinutePageViews = 0;
 
-    pageViews.push(now);
+    let url = req.originalUrl;
 
-    /**
-     * Simple and fast solution:
-     * Iterate over array from tail to head,
-     * increment page views counter while diff in seconds least than 60
-     */
-    // for (let i = pageViews.length - 1; i > -1; i--) {
-    //     let diffInSeconds = (now - pageViews[i]) / 1000;
-    //
-    //     if (diffInSeconds > 60) {
-    //         break;
-    //     }
-    //
-    //     pastMinutePageViews++;
-    // }
+    pageViewsCounter.addPageView(url);
 
-
-    /**
-     * Very simple solution with arrow function, may be slow, when array is big
-     */
-    pastMinutePageViews = pageViews.filter((v) => now - v < 60 * 1000).length;
+    let pastMinutePageViews = pageViewsCounter.getPageViewsOfPastMinute(url);
 
     res.send(`${pastMinutePageViews}`);
 });
